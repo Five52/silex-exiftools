1c22bfee27ab701b00f405cc694719118e92e693
Five52 <five52@laposte.net>
2016-12-09 23:19:47 +0100

    Add error display

--- app/app.php
+++ app/app.php
@@ -1,7 +1,11 @@
 <?php
+use Symfony\Component\Debug\ErrorHandler;
+use Symfony\Component\Debug\ExceptionHandler;
+
+ErrorHandler::register();
+ExceptionHandler::register();
 
 $app->register(new Silex\Provider\TwigServiceProvider(), [
     "twig.path" => __DIR__."/../views/"
 ]);
 $app['twig']->addGlobal('root', str_replace('index.php', '', $_SERVER['SCRIPT_NAME']));
-
