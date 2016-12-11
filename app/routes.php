689cab291412dfbbffec4489fcfaff4771301bc8
Five52 <five52@laposte.net>
2016-12-11 18:02:51 +0100

    Create /add route

--- app/routes.php
+++ app/routes.php
@@ -1,14 +1,20 @@
 <?php
 
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Validator\Constraints as Assert;
 
 use ExifTools\ExifTools;
+
+define('FILEDIR', __DIR__.'/../web/files');
 
 $app->match('/', function(Request $request) use ($app) {
     $dirname = "../web/files/";
     $images = glob($dirname . "*.jpg");
 
-    return $app['twig']->render('index.html.twig', array(
+    return $app->render('index.html.twig', array(
         'images' => $images
         ));
 })
@@ -26,7 +32,7 @@ $app->match('/ajout-image', function(Request $request) use ($app) {
         $file->move(__DIR__."/../web/files", $fileName);
         return $app->redirect($app['url_generator']->generate('image_added'));
     }
-    return $app['twig']->render('add.html.twig');
+    return $app->render('add.html.twig');
 }, 'GET|POST')
 ->bind('add_image');
 
@@ -43,9 +49,40 @@ $app->get('/test', function(){
         echo '</pre>';
         $test = "fail";
     }
-    
+
     echo '<pre>';
         var_dump($test);
     echo '</pre>';
     return "doky";
 });
+
+$app->match('/add', function(Request $request) use ($app) {
+    // Create file upload form
+    $data = [];
+    $form = $app->form($data)
+        ->add('file', FileType::class, [
+            'constraints' => new Assert\Image(),
+            'label' => 'Image',
+            'required' => false
+        ])
+        ->add('submit', SubmitType::class, ['label' => 'Ajouter'])
+        ->getForm()
+    ;
+
+    $form->handleRequest($request);
+    if ($request->isMethod('POST') && $form->isValid()) {
+        $data = $form->getData();
+        $file = $data['file'];
+        do {
+            $fileName = md5(uniqid()) . '.' . $file->guessExtension();
+        } while (file_exists(FILEDIR . '/' . $fileName));
+
+        $file->move(FILEDIR, $fileName);
+        ExifTools::generateImgMeta($fileName);
+        return $app->redirect($app->path('image_added'));
+    }
+
+    return $app->render('add.html.twig', [
+        'form' => $form->createView(),
+    ]);
+}, 'GET|POST');
