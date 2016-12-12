3403ef9160a11e50d0ae01bb3f0e2f46ce6741a1
Five52 <five52@laposte.net>
2016-12-12 02:43:50 +0100

    Add update and js management

---
+++ web/js/meta-form.js
@@ -0,0 +1,28 @@
+let labels = document.querySelectorAll('label');
+
+let fieldsets = {};
+
+labels.forEach((label) => {
+    const splitted = label.textContent.split(':');
+    if (splitted.length === 2) {
+        const name = splitted[0];
+        if (!fieldsets.hasOwnProperty(name)) {
+            let fieldset = document.createElement('fieldset');
+            fieldset.innerHTML = '<legend>' + name;
+            fieldsets[name] = fieldset;
+        }
+        label.textContent = splitted[1].trim();
+        fieldsets[name].appendChild(label.parentNode);
+    }
+});
+
+let form = document.querySelector('form');
+let submit = document.querySelector('button[type="submit"]');
+for (let key in fieldsets) {
+    if (fieldsets.hasOwnProperty(key)) {
+        console.log(form);
+        console.log(submit);
+        console.log(fieldsets[key]);
+        form.firstChild.insertBefore(fieldsets[key], submit.parentNode);
+    }
+}
