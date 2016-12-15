4eaa154907491e5ec4879212c9eb8f9a384df13c
plabadille <plabadille@gmail.com>
2016-12-15 19:52:56 +0100

    optimize consistency-handler, clear the code and send the form if user choose a button

--- web/js/consistency-handler.js
+++ web/js/consistency-handler.js
@@ -1,33 +1,78 @@
-//IPTC to check const
-const objectNameIptc = document.querySelector('[name="form\[IPTC:ObjectName\]"]');
-const headlineIptc = document.querySelector('[name="form\[IPTC:Headline\]"]');
-const byLineIptc = document.querySelector('[name="form\[IPTC:By-line\]"]');
-const creditIptc = document.querySelector('[name="form\[IPTC:Credit\]"]');
-const sourceIptc = document.querySelector('[name="form\[IPTC:Source\]"]');
-const captionAbstractIptc = document.querySelector('[name="form\[IPTC:Caption-Abstract\]"]');
-const dateCreatedIptc = document.querySelector('[name="form\[IPTC:DateCreated\]"]');
-const provinceStateIptc = document.querySelector('[name="form\[IPTC:Province-State\]"]');
-const copyrightNoticeIptc = document.querySelector('[name="form\[IPTC:CopyrightNotice\]"]');
-const cityIptc = document.querySelector('[name="form\[IPTC:City\]"]');
-const countryPrimaryIptc = document.querySelector('[name="form\[IPTC:Country-PrimaryLocationName\]"]');
+//We first need to check if commune metadata are both present
+const presentMetadata = [];
 
-//XMP to check const
-const titleXmp = document.querySelector('[name="form\[XMP:Title\]"]');
-const headlineXmp = document.querySelector('[name="form\[XMP:Headline\]"]');
-const creatorXmp = document.querySelector('[name="form\[XMP:Creator\]"]');
-const creditXmp = document.querySelector('[name="form\[XMP:Credit\]"]');
-const sourceXmp = document.querySelector('[name="form\[XMP:Source\]"]');
-const descriptionXmp = document.querySelector('[name="form\[XMP:Description\]"]');
-const dateCreatedXmp = document.querySelector('[name="form\[XMP:DateCreated\]"]');
-const stateXmp = document.querySelector('[name="form\[XMP:State\]"]');
-const usageTermsXmp = document.querySelector('[name="form\[XMP:UsageTerms\]"]');
-const cityXmp = document.querySelector('[name="form\[XMP:City\]"]');
-const countryXmp = document.querySelector('[name="form\[XMP:Country\]"]');
+if (document.querySelector('[name="form\[XMP:Title\]"]') !== null && document.querySelector('[name="form\[IPTC:ObjectName\]"]') !== null) {
+    presentMetadata.push({
+        xmp : document.querySelector('[name="form\[XMP:Title\]"]'), 
+        iptc : document.querySelector('[name="form\[IPTC:ObjectName\]"]')
+    });
+}
+if (document.querySelector('[name="form\[XMP:Headline\]"]') !== null && document.querySelector('[name="form\[IPTC:Headline\]"]') !== null) {
+    presentMetadata.push({
+        xmp : document.querySelector('[name="form\[XMP:Headline\]"]'), 
+        iptc : document.querySelector('[name="form\[IPTC:Headline\]"]')
+    });
+}
+if (document.querySelector('[name="form\[XMP:Creator\]"]') !== null && document.querySelector('[name="form\[IPTC:By-line\]"]') !== null) {
+    presentMetadata.push({
+        xmp : document.querySelector('[name="form\[XMP:Creator\]"]'), 
+        iptc : document.querySelector('[name="form\[IPTC:By-line\]"]')
+    });
+}
+if (document.querySelector('[name="form\[XMP:Credit\]"]') !== null && document.querySelector('[name="form\[IPTC:Credit\]"]') !== null) {
+    presentMetadata.push({
+        xmp : document.querySelector('[name="form\[XMP:Credit\]"]'), 
+        iptc : document.querySelector('[name="form\[IPTC:Credit\]"]')
+    });
+}
+if (document.querySelector('[name="form\[XMP:Source\]"]') !== null && document.querySelector('[name="form\[IPTC:Source\]"]') !== null) {
+    presentMetadata.push({
+        xmp : document.querySelector('[name="form\[XMP:Source\]"]'), 
+        iptc : document.querySelector('[name="form\[IPTC:Source\]"]')
+    });
+}
+if (document.querySelector('[name="form\[XMP:Description\]"]') !== null && document.querySelector('[name="form\[IPTC:Caption-Abstract\]"]') !== null) {
+    presentMetadata.push({
+        xmp : document.querySelector('[name="form\[XMP:Description\]"]'), 
+        iptc : document.querySelector('[name="form\[IPTC:Caption-Abstract\]"]')
+    });
+}
+if (document.querySelector('[name="form\[XMP:DateCreated\]"]') !== null && document.querySelector('[name="form\[IPTC:DateCreated\]"]') !== null) {
+    presentMetadata.push({
+        xmp : document.querySelector('[name="form\[XMP:DateCreated\]"]'), 
+        iptc : document.querySelector('[name="form\[IPTC:DateCreated\]"]')
+    });
+}
+if (document.querySelector('[name="form\[XMP:State\]"]') !== null && document.querySelector('[name="form\[IPTC:Province-State\]"]') !== null) {
+    presentMetadata.push({
+        xmp : document.querySelector('[name="form\[XMP:State\]"]'), 
+        iptc : document.querySelector('[name="form\[IPTC:Province-State\]"]')
+    });
+}
+if (document.querySelector('[name="form\[XMP:UsageTerms\]"]') !== null && document.querySelector('[name="form\[IPTC:CopyrightNotice\]"]') !== null) {
+    presentMetadata.push({
+        xmp : document.querySelector('[name="form\[XMP:UsageTerms\]"]'), 
+        iptc : document.querySelector('[name="form\[IPTC:CopyrightNotice\]"]')
+    });
+}
+if (document.querySelector('[name="form\[XMP:City\]"]') !== null && document.querySelector('[name="form\[IPTC:City\]"]') !== null) {
+    presentMetadata.push({
+        xmp : document.querySelector('[name="form\[XMP:City\]"]'), 
+        iptc : document.querySelector('[name="form\[IPTC:City\]"]')
+    });
+}
+if (document.querySelector('[name="form\[XMP:Country\]"]') !== null && document.querySelector('[name="form\[IPTC:Country-PrimaryLocationName\]"]') !== null) {
+    presentMetadata.push({
+        xmp : document.querySelector('[name="form\[XMP:Country\]"]'), 
+        iptc : document.querySelector('[name="form\[IPTC:Country-PrimaryLocationName\]"]')
+    });
+}
 
+//used to don't check the form if user apply edit by button.
 let checkConsistency = true;
 
-//form is already define in meta-form.js
 if (checkConsistency) {
+    //let form is already define in meta-form.js
     form.onsubmit = function(event) {
         event.preventDefault();
         let consistency = consistencyCheck();
@@ -77,148 +122,24 @@ function inconsistencyButtonListener() {
 }
     
 function applyConsistencyMesure(type) {
-
-    if (type == "applyInconsistency") {
-        //we indicate to don't check consistency again and send the form
-        checkConsistency = false;
-        form.submit();
-        return null;
-    }
-            
-    //we normalize the form with the choosen metadata type
-    //we don't send automaticaly the form to let the user check.
-    if (objectNameIptc !== null && titleXmp !== null) {
-        if (type == "applyXmp") {
-            objectNameIptc.value = titleXmp.value;
-        } else if (type == "applyIptc") {
-            titleXmp.value = objectNameIptc.value;
+    if (type != "applyInconsistency") {     
+        //we normalize the form with the choosen metadata type
+        for (let i = 0; i < presentMetadata.length ; i++) {
+            if (type == "applyXmp") {
+                presentMetadata[i].iptc.value = presentMetadata[i].xmp.value;
+            } else if (type == "applyIptc") {
+                presentMetadata[i].xmp.value = presentMetadata[i].iptc.value;
+            } 
         }
     }
-    if (headlineIptc !== null && headlineXmp !== null) {
-        if (type == "applyXmp") {
-            headlineIptc.value = headlineXmp.value;
-        } else if (type == "applyIptc") {
-            headlineXmp.value = headlineIptc.value;
-        }
-    }
-    if (byLineIptc !== null && creatorXmp !== null) {
-        if (type == "applyXmp") {
-            byLineIptc.value = creatorXmp.value;
-        } else if (type == "applyIptc") {
-            creatorXmp.value = byLineIptc.value;
-        }
-    }
-    if (creditIptc !== null && creditXmp !== null) {
-        if (type == "applyXmp") {
-            creditIptc.value = creditXmp.value;
-        } else if (type == "applyIptc") {
-            creditXmp.value = creditIptc.value;
-        }
-    }
-    if (sourceIptc !== null && sourceXmp !== null) {
-        if (type == "applyXmp") {
-            sourceIptc.value = sourceXmp.value;
-        } else if (type == "applyIptc") {
-            sourceXmp.value = sourceIptc.value;
-        }
-    }
-    if (captionAbstractIptc !== null && descriptionXmp !== null) {
-        if (type == "applyXmp") {
-            captionAbstractIptc.value = descriptionXmp.value;
-        } else if (type == "applyIptc") {
-            descriptionXmp.value = captionAbstractIptc.value;
-        }
-    }
-    if (dateCreatedIptc !== null && dateCreatedXmp !== null) {
-        if (type == "applyXmp") {
-            dateCreatedIptc.value = dateCreatedXmp.value;
-        } else if (type == "applyIptc") {
-            dateCreatedXmp.value = dateCreatedIptc.value;
-        }
-    }
-    if (provinceStateIptc !== null && stateXmp !== null) {
-        if (type == "applyXmp") {
-            provinceStateIptc.value = stateXmp.value;
-        } else if (type == "applyIptc") {
-            stateXmp.value = provinceStateIptc.value;
-        }
-    }
-    if (copyrightNoticeIptc !== null && usageTermsXmp !== null) {
-        if (type == "applyXmp") {
-            copyrightNoticeIptc.value = usageTermsXmp.value;
-        } else if (type == "applyIptc") {
-            usageTermsXmp.value = copyrightNoticeIptc.value;
-        }
-    }
-    if (cityIptc !== null && cityXmp !== null) {
-        if (type == "applyXmp") {
-            cityIptc.value = cityXmp.value;
-        } else if (type == "applyIptc") {
-            cityXmp.value = cityIptc.value;
-        }
-    }
-    if (countryPrimaryIptc !== null && countryXmp !== null) {
-        if (type == "applyXmp") {
-            countryPrimaryIptc.value = countryXmp.value;
-        } else if (type == "applyIptc") {
-            countryXmp.value = countryPrimaryIptc.value;
-        }
-    }
+    //we indicate to don't check consistency again and send the form
+    checkConsistency = false;
+    form.submit();
 }
 
 function consistencyCheck() {
-    if (objectNameIptc !== null && titleXmp !== null) {
-        if (objectNameIptc.value != titleXmp.value) {
-            return false;
-        }
-    }
-    if (headlineIptc !== null && headlineXmp !== null) {
-        if (headlineIptc.value != headlineXmp.value) {
-            return false;
-        }
-    }
-    if (byLineIptc !== null && creatorXmp !== null) {
-        if (byLineIptc.value != creatorXmp.value) {
-            return false;
-        }
-    }
-    if (creditIptc !== null && creditXmp !== null) {
-        if (creditIptc.value != creditXmp.value) {
-            return false;
-        }
-    }
-    if (sourceIptc !== null && sourceXmp !== null) {
-        if (sourceIptc.value != sourceXmp.value) {
-            return false;
-        }
-    }
-    if (captionAbstractIptc !== null && descriptionXmp !== null) {
-        if (captionAbstractIptc.value != descriptionXmp.value) {
-            return false;
-        }
-    }
-    if (dateCreatedIptc !== null && dateCreatedXmp !== null) {
-        if (dateCreatedIptc.value != dateCreatedXmp.value) {
-            return false;
-        }
-    }
-    if (provinceStateIptc !== null && stateXmp !== null) {
-        if (provinceStateIptc.value != stateXmp.value) {
-            return false;
-        }
-    }
-    if (copyrightNoticeIptc !== null && usageTermsXmp !== null) {
-        if (copyrightNoticeIptc.value != usageTermsXmp.value) {
-            return false;
-        }
-    }
-    if (cityIptc !== null && cityXmp !== null) {
-        if (cityIptc.value != cityXmp.value) {
-            return false;
-        }
-    }
-    if (countryPrimaryIptc !== null && countryXmp !== null) {
-        if (countryPrimaryIptc.value != countryXmp.value) {
+    for (let i = 0; i < presentMetadata.length ; i++) {
+        if (presentMetadata[i].iptc.value != presentMetadata[i].xmp.value) {
             return false;
         }
     }
